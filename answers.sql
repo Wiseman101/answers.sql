3ac99dbb51afc8c992b45e847be156b70cda1d14
Wiseman101 <hillarywafula45@gmail.com>
2025-03-21 11:19:09 -0700

    sql answers

---
+++ answers.sql
@@ -0,0 +1,13 @@
+Write an SQL query to create a new database called salesDB.
+
+
+-- Query to create a new database called 'salesDB'
+CREATE DATABASE salesDB;
+-- The above command creates a new database named 'salesDB'. 
+-- Ensure you have the necessary permissions to create a database.
+
+Write an SQL query to drop (delete) the database called demo.
+-- Query to drop (delete) the database called 'demo'
+DROP DATABASE demo;
+-- The above command deletes the database named 'demo'. 
+-- Use this command cautiously as it will permanently remove the database and all its data.
